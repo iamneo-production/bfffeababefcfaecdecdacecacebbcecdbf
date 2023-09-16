50d2efa1dad197abc1219d1e36b489a9b6b5da76
Iamneo <techadmin@iamneo.ai>
2023-09-16 08:12:52 +0000

    Backup Commit

--- 1.ps1
+++ 1.ps1
@@ -1,4 +1,4 @@
-$folderpath = "home\coder\project\workspace\folder"
-$filepath= "home\coder\project\workspace\file.txt"
+$folderpath = "home/coder/project/workspace/folder"
+$filepath= "home\coder\project/workspace/file.txt"
 new-item -itemtype directory -path $folderpath
 new-item -itemtype file -path $filepath
